f7c357994e4fecd646ed328a5a125a85f8145f91
purusottam <purusottam16@gmail.com>
2017-12-19 21:00:41 +0530

    working code

---
+++ microservice-client/src/main/java/com/spring/cloud/microserviceclient/AppConfig.java
@@ -0,0 +1,17 @@
+package com.spring.cloud.microserviceclient;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+	@LoadBalanced
+	@Bean	
+	public RestTemplate getRestTemplate(){
+		return new RestTemplate();
+		
+	}
+
+}
